1fecf2016bd70ddabc98693f8184e7c5e431845e
Sander Tonheim <tonheims98@gmail.com>
2026-05-08 12:28:19 +0200

    added delete func

--- database/services.js
+++ database/services.js
@@ -1,6 +1,6 @@
 async function getUserData(connection, email) {
 	const [results] = await connection.query(
-		`SELECT * FROM user WHERE email = "${email}"`,
+		`SELECT * FROM user WHERE email = "${email}"`
 	);
 	return results;
 }
@@ -17,9 +17,16 @@ async function insertIntoBistandDatabase(connection, text, email) {
 
 async function getUserText(connection, email) {
 	const [results] = await connection.query(
-		`SELECT * FROM question WHERE email = "${email}"`,
+		`SELECT * FROM question WHERE email = "${email}"`
 	);
 	return results;
+}
+
+async function deleteUser(email) {
+	const deleteUserQuery = "DELETE FROM user WHERE email = ?";
+	connection.execute(deleteUserQuery, [email]);
+
+	connection.end();
 }
 module.exports = {
 	getUserData,
