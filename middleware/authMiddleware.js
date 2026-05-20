c7e453059efaac54a9ffb17bbcaa6246afa0a40c
Sander Tonheim <tonheims98@gmail.com>
2026-05-20 12:08:48 +0200

    updated route name

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,7 +2,7 @@ function isAuthenticated(req, res, next) {
     if (req.session && req.session.email) {
         next();
     } else {
-        res.redirect("/innlogging")
+		res.redirect("/signin");
     }
 }
 
